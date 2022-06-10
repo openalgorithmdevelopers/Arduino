98287f70da421888104a78c9e063d8376dddeefa
bhupendra <bhupendra.bisht59.com>
2022-06-10 13:48:30 +0530

    Done: data sending and recieving over MQTT

--- MQTT_Arduino_flask_Data_Transmission/MQTT_Arduino_flask_Data_Transmission.ino
+++ MQTT_Arduino_flask_Data_Transmission/MQTT_Arduino_flask_Data_Transmission.ino
@@ -34,9 +34,30 @@ unsigned long lastMsg = 0;
 #define MSG_BUFFER_SIZE	(50)
 char msg[MSG_BUFFER_SIZE];
 
+// Control message byte from flask/python
+//# 0000 00 00   #0 # do nothing or future use
+//# 0000 01 01   #5  # default short duration and low quality
+//# 0000 01 10   #6  # short duration and medium quality
+//# 0000 01 11   #7 # short duration and high quality
+//# 0000 10 01   #9 # medium duration and low quality
+//# 0000 10 10   #10 # medium duration and medium quality
+//# 0000 10 11   #11  # medium duration and high quality
+//# 0000 11 01   #13 # long duration and low quality
+//# 0000 11 10   #14 # long duration and medium quality
+//# 0000 11 11   #15  # long duration and high quality  
+
 int SIGNAL_DURATION_MEDIUM = 1000;  //medium duration is considered as 1 seconds
 int SIGNAL_DURATION_SHORT = 100;  //short duration is considered as 0.1 seconds
-int SIGNAL_DURATION_LONG = 10000;  //long duration is considered as 10seconds
+int SIGNAL_DURATION_LONG = 5000;  //long duration is considered as 5 seconds
+
+//Higher the signal quality, lesser the delay in sampling
+int SIGNAL_QUALITY_LOW = 2;    //delay in milliseconds, corresponds to 500Hz
+int SIGNAL_QUALITY_MEDIUM = 1;  //delay in milliseconds, corresponds to 1KHz
+float SIGNAL_QUALITY_HIGH = 0.5;  //delay in milliseconds, corresponds to 2KHz
+
+bool REPEAT_CAPTURE_AND_SEND = false;
+int currentSignalDuration = SIGNAL_DURATION_SHORT;
+float currentSignalQuality = SIGNAL_QUALITY_MEDIUM;
 
 void setup_wifi() {
 
@@ -66,13 +87,71 @@ void callback(char* topic, byte* payload, unsigned int length) {
   Serial.print("Message arrived [");
   Serial.print(topic);
   Serial.print("] ");
+  String signalParams = "";
   for (int i = 0; i < length; i++) {
-    Serial.print((char)payload[i]);
+//    Serial.print((char)payload[i]);
+      signalParams += (char)payload[i];
   }
   Serial.println();
-
+  setSignalParams(signalParams);
+  REPEAT_CAPTURE_AND_SEND = true;
 }
 
+void setSignalParams(String signalParams)
+{
+  int params = signalParams.toInt();
+  Serial.println("param recieved as = ");
+  Serial.print(params);
+  
+  if(params == 5)
+  {
+    currentSignalDuration = SIGNAL_DURATION_SHORT;
+    currentSignalQuality = SIGNAL_QUALITY_LOW;
+  }
+  else if(params == 6)
+  {
+    currentSignalDuration = SIGNAL_DURATION_SHORT;
+    currentSignalQuality = SIGNAL_QUALITY_MEDIUM;
+  }
+  else if(params == 7)
+  {
+    currentSignalDuration = SIGNAL_DURATION_SHORT;
+    currentSignalQuality = SIGNAL_QUALITY_HIGH;
+  }
+  else if(params == 9)
+  {
+    currentSignalDuration = SIGNAL_DURATION_MEDIUM;
+    currentSignalQuality = SIGNAL_QUALITY_LOW;
+  }
+  else if(params == 10)
+  {
+    currentSignalDuration = SIGNAL_DURATION_MEDIUM;
+    currentSignalQuality = SIGNAL_QUALITY_MEDIUM;
+  }
+  else if(params == 11)
+  {
+    currentSignalDuration = SIGNAL_DURATION_MEDIUM;
+    currentSignalQuality = SIGNAL_QUALITY_HIGH;
+  }
+  else if(params == 13)
+  {
+    currentSignalDuration = SIGNAL_DURATION_LONG;
+    currentSignalQuality = SIGNAL_QUALITY_LOW;
+  }
+  else if(params == 14)
+  {
+    currentSignalDuration = SIGNAL_DURATION_LONG;
+    currentSignalQuality = SIGNAL_QUALITY_MEDIUM;
+  }
+  else if(params == 15)
+  {
+    currentSignalDuration = SIGNAL_DURATION_LONG;
+    currentSignalQuality = SIGNAL_QUALITY_HIGH;
+  }
+  Serial.println("Current duration and quality set to ");
+  Serial.println(currentSignalDuration);
+  Serial.println(currentSignalQuality);
+}
 void reconnect() {
   // Loop until we're reconnected
   while (!client.connected()) {
@@ -81,15 +160,11 @@ void reconnect() {
     String clientId = "ESP8266Client-";
     clientId += String(random(0xffff), HEX);
     // Attempt to connect
-//    if (client.connect(clientId.c_str())) {
       if (client.connect(clientId.c_str()), "tornado", "") {
       Serial.println("connected");
       // Once connected, publish an announcement...
       client.publish("/DS/Arduino", "Digital Stethoscope sending.....");
-      // ... and resubscribe
-//      client.subscribe("inTopic");
-        //client.subscribe("/leds/esp8266");
-        client.subscribe("/DS/flask");        
+      client.subscribe("/DS/flask");        
     } else {
       Serial.print("failed, rc=");
       Serial.print(client.state());
@@ -104,11 +179,9 @@ void setup() {
   pinMode(BUILTIN_LED, OUTPUT);     // Initialize the BUILTIN_LED pin as an output
   Serial.begin(115200);
 
-
   setup_wifi();
   client.setServer(mqtt_server, 1883);
-  client.setCallback(callback);
-  
+  client.setCallback(callback);  
     
   if (!client.connected()) {
     reconnect();
@@ -116,7 +189,6 @@ void setup() {
   client.loop();
 }
 
-bool REPEAT_CAPTURE_AND_SEND = true;
 void loop() {
   if(REPEAT_CAPTURE_AND_SEND)
   {
@@ -154,24 +226,26 @@ void capture_and_write_signal(){
     Serial.println("File write failed");
   }  
 
+  int dataPoints = currentSignalDuration/currentSignalQuality;
   int loopCounter = 0;
-  while(loopCounter < SIGNAL_DURATION_SHORT){
+  while(loopCounter <= dataPoints){
     loopCounter += 1;  
     DS_signal = String(loopCounter) + ":";
 
     int sample = analogRead(A0);
     Serial.print(sample);
     DS_signal += String(sample) + ",";
-    
+      
     if (file.print(DS_signal)) {
       Serial.println("File was written with ");
       Serial.println(DS_signal);
-    } else {
+    } 
+    else {
       Serial.println("File write failed");
     }
-    delay(1); 
-  }
-  
+    delay(currentSignalQuality); 
+//    delay(2); 
+  }  
   if (file.print(":-5:")) { //end of signal
     Serial.println("File was written with End Character");
   } else {
@@ -179,8 +253,9 @@ void capture_and_write_signal(){
   }
 
   file.close();
-
   Serial.println("Signal recording on a file completed..");
+  Serial.println("signal quality = ");
+  Serial.println(currentSignalQuality);
 }
 
 void Send_File_MQTT(){
